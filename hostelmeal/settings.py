0e9f9864b10a16ca91ecf86edc2a6b9a4575a880
Jafor Sadik Tutul <tutul@ucasbd.com>
2025-10-08 17:35:55 +0600

    sded

--- hostelmeal/settings.py
+++ hostelmeal/settings.py
@@ -120,7 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+
+# Absolute path where collectstatic will gather all static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Optional: where you keep app-specific static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
